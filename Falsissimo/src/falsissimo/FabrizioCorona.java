658f01d1e079f265cd4f8951e878036d5cee8885
angelica <pazzagli.angelica@franchettisalviani.net>
2026-03-05 12:38:47 +0100

    Classi principali e StartForm iniziale

---
+++ Falsissimo/src/falsissimo/FabrizioCorona.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package falsissimo;
+
+/**
+ *
+ * @author pazzagli.angelica
+ */
+public abstract class FabrizioCorona {
+    
+}
