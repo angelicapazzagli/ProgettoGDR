5b4cd291fb09ab2fc52e6c06a739f521acd54ab0
angelica <pazzagli.angelica@franchettisalviani.net>
2026-03-10 13:28:58 +0100

    Prima impostazione classe astratta Personaggio

--- Falsissimo/src/falsissimo/FabrizioCorona.java
+++ Falsissimo/src/falsissimo/FabrizioCorona.java
@@ -9,5 +9,29 @@ package falsissimo;
  * @author pazzagli.angelica
  */
 public abstract class FabrizioCorona {
+    protected String nome;
+    protected int soldi;
+    protected int fama;
+    protected int astuzia;
+    protected Abilità abilità;
     
+    public FabrizioCorona(int soldi, int fama, int astuzia, Abilità abilità) {
+        this.soldi = soldi;
+        this.fama = fama;
+        this.astuzia = astuzia;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setAbilità(Abilità abilità) {
+        this.abilità = abilità;
+    }
+    
+    public abstract void usaAbilità();
 }
